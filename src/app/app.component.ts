5b1bf68d78d6957c81e380f94aa78026054b4744
Belal mohamed <belal2003mohamed@gmail.com>
2024-07-28 12:48:24 +0300

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'weather-app';
+  weatherData: any;
+  city: string = '';
+  private apiKey = '584ca1255e18eb76e4f940baf93193ba';
+  private apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) {}
+
+  getWeather() {
+    if (this.city.trim()) {
+      this.fetchWeather(this.city).subscribe(
+        (data) => {
+          this.weatherData = data;
+        },
+        (error) => {
+          console.error('Error fetching weather data', error);
+          this.weatherData = null;
+        }
+      );
+    }
+  }
+
+  fetchWeather(city: string): Observable<any> {
+    return this.http.get<any>(
+      `${this.apiUrl}?q=${city}&appid=${this.apiKey}&units=metric`
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
